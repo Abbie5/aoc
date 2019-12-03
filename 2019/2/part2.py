315c86a4fbcc8d17104302866ee9281f8e562bd9
Tom Ient <tom@ient.me>
2019-12-03 10:15:49 +0000

    Add day 2 part 2 solution

---
+++ 2019/2/part2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import intcode
+
+with open("input", "r") as f:
+    program = list(map(int, f.read().split(",")))
+
+def find_input(desired_result, initial_program):
+    for noun in range(0,100):
+        for verb in range(0,100):
+            program = initial_program.copy()
+
+            program[1] = noun
+            program[2] = verb
+
+            result = intcode.compute(program)
+
+            if result == desired_result:
+                return noun * 100 + verb
+
+print(find_input(19690720, program))
