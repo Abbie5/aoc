e46bdd19b0b78da288192dc8e3cb5f6ac850ad67
Tom Ient <tom@ient.me>
2019-12-03 18:40:41 +0000

    Day 3 part 2: don't define a function when it's only called once

--- 2019/3/part1.py
+++ 2019/3/part1.py
@@ -6,20 +6,16 @@ with open("input", "r") as f:
     wires = parse(f)
 
 
-
-def least_manhattan_distance(origin, intersects):
-    least_distance = None
-    for intersect in intersects:
-        coordinate = intersect.get_coordinate()
-        distance = manhattan_distance(origin, coordinate)
-        if (least_distance == None) or (distance < least_distance):
-            least_distance = distance
-
-    return least_distance
-
 origin = Coordinate(0,0)
 wire1 = wires[0]
 wire2 = wires[1]
 intersects = find_intersects(wire1, wire2)
-least_distance = least_manhattan_distance(origin, intersects)
+
+least_distance = None
+for intersect in intersects:
+    coordinate = intersect.get_coordinate()
+    distance = manhattan_distance(origin, coordinate)
+    if (least_distance == None) or (distance < least_distance):
+        least_distance = distance
+
 print(least_distance)
