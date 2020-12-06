fc668b1e88512c82edd46aac15d956b33787e55c
Abigail Ient <abigail@ient.me>
2020-12-06 10:57:59 +0000

    add 2020 day 6 parts 1 & 2 solutions

---
+++ 2020/day6/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let _answers = fs::read_to_string("input.txt")?;
+    let answers = _answers
+        .split("\n\n")
+        .map(|s| s.trim())
+        .collect::<Vec<&str>>();
+
+    let total_part1 = answers
+        .iter()
+        .map(|s| {
+            s.split_whitespace()
+                .collect::<String>()
+                .chars()
+                .collect::<HashSet<char>>()
+                .len()
+        })
+        .sum::<usize>();
+
+    let total_part2 = answers
+        .iter()
+        .map(|s| {
+            let group = s
+                .split("\n")
+                .map(|t| t.chars().collect())
+                .collect::<Vec<Vec<char>>>();
+
+            let mut questions: HashSet<&char> = group.iter().flatten().collect();
+            for person in group.iter() {
+                questions.retain(|k| person.contains(k));
+            }
+            questions.len()
+        })
+        .sum::<usize>();
+
+    println!("{}", total_part1);
+    println!("{}", total_part2);
+
+    Ok(())
+}
